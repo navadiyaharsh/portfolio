195031ec1a767c6e29f732314b72fc1645d91a28
hn2276 <56693520+navadiyaharsh111@users.noreply.github.com>
2024-04-10 20:33:50 -0400

    Update skills script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Data Science", "backend development", "Cloud Computing", "Machine Learning"],
+    strings: ["Machine Learning Engineer", "AI Engineer", "Data Scientist", "Cloud Engineer", "Backend Developer"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
