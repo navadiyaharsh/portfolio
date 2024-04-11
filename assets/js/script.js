caacc77041a4ba936f9fd4617439460ed6e6e4c8
navadiyaharsh111 <navadiyaharsh111@gmail.com>
2024-04-11 00:25:02 -0400

    Updated Theme

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,46 @@
+// Initialising the canvas
+var canvas = document.querySelector('canvas'),
+    ctx = canvas.getContext('2d');
+
+// Setting the width and height of the canvas
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// Setting up the letters
+var letters = '૨૩૪૫૬૭૮૯કખગઘઙચછજઝઞટઠડઢણતથદધનપફબભમયરલળવશષસહ૦૧';
+letters = letters.split('');
+
+// Setting up the columns
+var fontSize = 30,
+    columns = Math.floor(canvas.width / fontSize);
+
+// Setting up the drops
+var drops = [];
+for (var i = 0; i < columns; i++) {
+    drops[i] = Math.floor(Math.random() * canvas.height / fontSize);
+}
+
+// Setting up the draw function
+function draw() {
+    ctx.fillStyle = 'rgba(0, 0, 0, 1)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    for (var i = 0; i < drops.length; i++) {
+        var text = letters[Math.floor(Math.random() * letters.length)];
+        ctx.fillStyle = '#0f0';
+        ctx.font = fontSize + 'px Arial';
+        ctx.fillText(text, i * fontSize, drops[i] * fontSize);
+        drops[i]++;
+        if (drops[i] * fontSize > canvas.height) {
+            drops[i] = 0;
+        }
+    }
+}
+
+// Loop the animation
+setInterval(draw, 200);
+
+
+
 $(document).ready(function () {
 
     $('#menu').click(function () {
